491778afbc35ce508185e7254387f7d249464b03
Bhumesh Mahajan <167601736+Bhumesh01@users.noreply.github.com>
2026-06-21 23:37:24 +0530

    deploy

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,12 +1,10 @@
 import AnimatedBackground from "../components/home/AnimatedBackground";
 import Hero from "../components/home/Hero";
-import Navbar from "../components/home/Navbar";
 
 export default function Home() {
   return (
     <div className=" relative min-h-screen overflow-hidden bg-linear-to-br from-background via-background to-primary/5 text-foreground ">
       <AnimatedBackground />
-      {/* <Navbar /> */}
       <main
         className=" relative z-10 mx-auto max-w-7xl px-8 "
       >
